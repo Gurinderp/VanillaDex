56ffe941287216ee875b39a26038b3ac78936fbf
rednirug <sgurinderpreet@gmail.com>
2021-05-12 23:19:40 -0700

    express POST added to create pokemon data

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -1,60 +1,55 @@
 const mongoose = require("mongoose");
 
 const pokemonSchema = new mongoose.Schema({
-	information: [
-		{
-			name: {
-				type: String,
-				required: true,
-			},
-			dexNumber: {
-				type: Number,
-				required: true,
-			},
-			primaryType: {
-				type: String,
-				required: true,
-			},
-			secondaryType: {
-				type: String,
-				required: true,
-			},
-			description: {
-				type: String,
-				required: true,
-			},
-			devolvedForm: String,
-			evolvedForm: String,
+	information: {
+		name: {
+			type: String,
+			required: true,
 		},
-	],
-	stats: [
-		{
-			health: {
-				type: Number,
-				required: true,
-			},
-			attack: {
-				type: Number,
-				required: true,
-			},
-			specialAttack: {
-				type: Number,
-				required: true,
-			},
-			defense: {
-				type: Number,
-				required: true,
-			},
-			specialDefense: {
-				type: Number,
-				required: true,
-			},
-			speed: {
-				type: Number,
-				required: true,
-			},
+		dexNumber: {
+			type: Number,
+			required: true,
 		},
-	],
+		primaryType: {
+			type: String,
+			required: true,
+		},
+		secondaryType: {
+			type: String,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		devolvedForm: String,
+		evolvedForm: String,
+	},
+	stats: {
+		health: {
+			type: Number,
+			required: true,
+		},
+		attack: {
+			type: Number,
+			required: true,
+		},
+		specialAttack: {
+			type: Number,
+			required: true,
+		},
+		defense: {
+			type: Number,
+			required: true,
+		},
+		specialDefense: {
+			type: Number,
+			required: true,
+		},
+		speed: {
+			type: Number,
+			required: true,
+		},
+	},
 });
 
 module.exports = mongoose.model("Pokemon", pokemonSchema);
--- server.js
+++ server.js
@@ -1,23 +1,83 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+// Models
+const Pokemon = require("./models/Pokemon");
+
+// dotenv configuration method
 dotenv.config();
 
+// capturing express() in app variable
 const app = express();
 
+// telling express to handle json data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// setting development environment variables
 const DB_URI = process.env.DB_URI;
 const PORT = process.env.PORT || 3000;
 
+// mongoose connecting to MongoDB database
 mongoose
 	.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((result) => console.log("Connected to Database"))
 	.catch((err) => console.log(err));
 
+// Beginning app on specified port
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
+// GET request for home page
 app.get("/", function (req, res) {
 	res.send("Hello PokeWorld!");
 });
+
+// GET request for pokedex page
+app.get("/pokedex", function (req, res) {
+	res.send("Hello Pokedex!");
+});
+
+// GET request for pokemon list
+app.get("/pokedex/pokemon", function (req, res) {
+	res.send("hello pokemon");
+});
+
+//
+// Need GET request to search in Pokedex
+//
+
+// POST request to register pokemon from front end
+app.post("/pokedex/register", async function (req, res) {
+	const pokemon = new Pokemon({
+		information: {
+			name: req.body.information.name,
+			dexNumber: req.body.information.dexNumber,
+			primaryType: req.body.information.primaryType,
+			secondaryType: req.body.information.secondaryType,
+			description: req.body.information.description,
+			devolvedForm: req.body.information.devolvedForm,
+			evolvedForm: req.body.information.evolvedForm,
+		},
+		stats: {
+			health: req.body.stats.health,
+			attack: req.body.stats.attack,
+			specialAttack: req.body.stats.specialAttack,
+			defense: req.body.stats.defense,
+			specialDefense: req.body.stats.specialDefense,
+			speed: req.body.stats.speed,
+		},
+	});
+	try {
+		const newPokemon = await pokemon.save();
+		res.send("Pokemon Added");
+	} catch (err) {
+		res.send(err);
+		console.log(err);
+	}
+});
+
+//
+//
+//
