ba30d25d36cac6b0ea0c3ff968a24d4517bacd68
rednirug <sgurinderpreet@gmail.com>
2022-05-05 01:06:06 -0700

    fixed dependency errors and added error messages for Pokemon Model

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -4,22 +4,24 @@ const pokemonSchema = new mongoose.Schema({
 	information: {
 		name: {
 			type: String,
-			required: true,
+			required: [true, "Pokemon Name is required."],
 		},
 		dexNumber: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Dex number is required."],
+			min: 0,
+			max: 151,
 		},
 		primaryType: {
 			type: String,
-			required: true,
+			required: [true, "Pokemon Primary type is required."],
 		},
 		secondaryType: {
 			type: String,
 		},
 		description: {
 			type: String,
-			required: true,
+			required: [true, "Pokemon Description is required."],
 		},
 		devolvedForm: String,
 		evolvedForm: String,
