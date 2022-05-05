845d3229232aa5b3a5adef202e84d405f3a25d2e
rednirug <sgurinderpreet@gmail.com>
2022-05-05 01:18:20 -0700

    Pokemon and Moves Model validation

--- models/Moves.js
+++ models/Moves.js
@@ -3,25 +3,31 @@ const mongoose = require("mongoose");
 const movesSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: true,
+		required: [true, "Move Name is required."],
 	},
 	type: {
 		type: String,
-		required: true,
+		required: [true, "Move Type is required."],
 	},
 	power: {
 		type: Number,
-		required: true,
+		required: [true, "Move Power is required."],
+		min: [1, "Power cannot be lower than 1."],
+		max: [200, "Invalid entry: Power too high."],
 	},
 	powerpoint: {
 		type: Number,
+		min: [1, "PP cannot be lower than 1."],
+		max: [50, "Invalid entry: Attack too high."],
 	},
 	accuracy: {
 		type: Number,
+		min: [1, "Accuracy cannot be lower than 1."],
+		max: [100, "Invalid entry: Accuracy too high."],
 	},
 	description: {
 		type: String,
-		required: true,
+		required: [true, "Move Description is required."],
 	},
 });
 
--- models/Pokemon.js
+++ models/Pokemon.js
@@ -9,8 +9,8 @@ const pokemonSchema = new mongoose.Schema({
 		dexNumber: {
 			type: Number,
 			required: [true, "Pokemon Dex number is required."],
-			min: 0,
-			max: 151,
+			min: [1, "Dex Number must be above Zero."],
+			max: [151, "First generation only goes up to 151."],
 		},
 		primaryType: {
 			type: String,
@@ -29,27 +29,39 @@ const pokemonSchema = new mongoose.Schema({
 	stats: {
 		health: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Health is required."],
+			min: [1, "Health cannot be lower than 1."],
+			max: [500, "Invalid entry: Health too high."],
 		},
 		attack: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Attack is required."],
+			min: [1, "Attack cannot be lower than 1."],
+			max: [500, "Invalid entry: Attack too high."],
 		},
 		specialAttack: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Special Attack is required."],
+			min: [1, "Special Attack cannot be lower than 1."],
+			max: [500, "Invalid entry: Special Attack too high."],
 		},
 		defense: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Defense is required."],
+			min: [1, "Defense cannot be lower than 1."],
+			max: [500, "Invalid entry: Defense too high."],
 		},
 		specialDefense: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Special Defense is required."],
+			min: [1, "Special Defense cannot be lower than 1."],
+			max: [500, "Invalid entry: Special Defense too high."],
 		},
 		speed: {
 			type: Number,
-			required: true,
+			required: [true, "Pokemon Speed is required."],
+			min: [1, "Speed cannot be lower than 1."],
+			max: [500, "Invalid entry: Speed too high."],
 		},
 	},
 });
